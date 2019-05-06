ad38f7c7caeae7d83f5d2ba0f88a21efe148d98c
glooooo <siriusbarberwoeltjen@gmail.com>
2019-05-06 16:35:14 -0600

    made it work

--- ShoeTest.java
+++ ShoeTest.java
@@ -2,11 +2,22 @@ import java.util.Scanner;
 public class ShoeTest{
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("How many decks do you want in the shoe?");
-		int decks = input.nextInt();
+		int decks, redraw;
+		do{
+			System.out.println("How many decks do you want in the shoe?");
+			decks = input.nextInt();
+			if(decks < 1 || decks > 10){
+				System.out.println("Please enter a valid number");
+			}
+		}while(decks < 1 || decks > 10);
 		Shoe shoe = new Shoe(decks);
-		System.out.println("What should the redraw limit be?");
-		int redraw = input.nextInt();
+		do{
+			System.out.println("What should the redraw limit be?");
+			redraw = input.nextInt();
+			if(redraw < 0 || redraw > 52*decks){
+				System.out.println("Please enter a valid input.");
+			}
+		}while(redraw < 0 || redraw > 52*decks);
 		Card card;
 		for(int i = 0; i < ((52*decks)-redraw); i++){
 			card = shoe.draw();
