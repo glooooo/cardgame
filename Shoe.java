04ff9f2d10c20e82415a4199860f2401cc6006d3
Ashlinn Barber-Woeltjen <siriusbarberwoeltjen@gmail.com>
2019-05-06 15:53:55 -0600

    added shoe

---
+++ Shoe.java
@@ -0,0 +1,27 @@
+import java.util.Vector;
+import java.lang.Math;
+public class Shoe{
+  private Vector<Deck> shoe;
+  int deckNum;
+  public Shoe(int deckNum){
+    this.deckNum = deckNum;
+    shoe = new Vector<Deck>();
+    for(int i = 0; i < deckNum; i++){
+      shoe.add(new Deck());
+    }
+    for(Deck deck : shoe){
+      deck.shuffle();
+    }
+  }
+  public Card draw(){
+    return shoe(Math.random(deckNum).draw());
+    for(Deck deck : shoe){
+      if(deck.getRemaining() = 0){
+        shoe.removeElement(deck);
+      }
+    }
+  }
+  public int getRemainingDecks(){
+    return shoe.size();
+  }
+}
